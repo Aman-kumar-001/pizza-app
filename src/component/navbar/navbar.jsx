b904165f0e0e7ada5ecbd9dacc85dc4e6e0862fe
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2023-12-08 16:58:49 +0530

    frame ready

---
+++ src/component/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+// import "../navbar/navbar.css";
+// import { IoMenu } from "react-icons/io5";
+
+
+function navbar(){
+return(
+    <div className="header">
+        hello
+   {/* <IoMenu/> */}
+    </div>
+)
+}
+export default navbar;
