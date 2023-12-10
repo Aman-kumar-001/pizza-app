c2940e422825bcce86ff20c6543f7259d9f007a1
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2023-12-11 03:06:37 +0530

    till slider

--- src/component/slider/slider.jsx
+++ src/component/slider/slider.jsx
@@ -2,41 +2,58 @@ import { FaArrowRight } from "react-icons/fa6";
 import { FaArrowLeft } from "react-icons/fa6";
 import { useState } from "react";
 import "../slider/slider.css";
-import IMG1 from "../../assests/pngwing.com1.png";
-import IMG2 from "../../assests/pngwing.com2.png";
-import IMG3 from "../../assests/pngwing.com3.png";
-function Slider(){
+function Slider() {
+  const sliderItems = [
+    {
+      key:"1",
+      img: "https://i.imgur.com/XWWRMjY.png"
+    },
+    {
+      key:'2',
+      img: "https://i.imgur.com/UYOyrA3.png"
+    },
+    // {
+    //   key:'3',
+    //   img: "https://i.imgur.com/hN3q1kn.png"
+    // }
+  ];
 
-    const[ slider , setSlider] = useState(0);
+  const [slider, setSlider] = useState(0);
 
-    const prevslider= () => {
-        setSlider(slider === 0 ? 2 : (prev) => prev - 1)
-        console.log("prev");
-    }
+  const prevslider = () => {
+    setSlider(slider === 0 ? 2 : (prev) => prev - 1);
+    console.log("prev");
+  };
 
-    const nextslider = () => {
-        setSlider(slider === 2 ? 0 : (prev) => prev + 1)
-        console.log("next");
-    }
-     
-    return (
-        <div className="slider">
-            <div className="container">
-                <div className="arrow1" onClick={prevslider}>
-                    <FaArrowLeft  className="Arr"/>
-               </div> 
-               <div className="image" >
-               <img src={IMG1} alt="" />
-               {/* <img src={IMG2} alt="" /> */}
-               {/* <img src={IMG3} alt="" /> */}
-                </div> 
-               <div className="arrow2" onClick={nextslider}>
-                 <FaArrowRight className="Arr"/>
-                 </div>
-            </div>
+  const nextslider = () => {
+    setSlider(slider === 2 ? 0 : (prev) => prev + 1);
+    console.log("next");
+  };
+
+  return (
+    <div className="slider">
+      <div className="container">
+        <div className="arrow1" onClick={prevslider}>
+          <FaArrowLeft className="Arr" />
         </div>
-    );
+        <div
+          className="wrappers"
+          style={{ transform: `translateX(-${slider * 60}vw)` }}
+        >
+          {sliderItems.map((item) => (
+            <div className="slide">
+              <div className="img-container">
+              <img src={item.img} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="arrow2" onClick={nextslider}>
+          <FaArrowRight className="Arr" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Slider;
-
