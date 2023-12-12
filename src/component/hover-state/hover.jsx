4fe35c29be1d1e4a5e46a1570c51dadf5a3883e9
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2023-12-12 19:54:16 +0530

    hover element

--- src/component/hover-state/hover.jsx
+++ src/component/hover-state/hover.jsx
@@ -4,10 +4,14 @@ function Hover(){
     return(
      <div className="Hover-box">
         <div className="Hover1">
+
+        </div>
+        <div className="Hover2">
+
+        </div>
+        <div className="Hover3">
             
         </div>
-        <div className="Hover2"></div>
-        <div className="Hover3"></div>
      </div>
     );
 }
