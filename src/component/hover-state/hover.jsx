aeab201c45985d9cea8d51e08ba1351c3a1feff6
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2023-12-13 00:03:37 +0530

    till hover container

--- src/component/hover-state/hover.jsx
+++ src/component/hover-state/hover.jsx
@@ -1,19 +1,35 @@
 import "../hover-state/hover.css";
+import ICE from "../../assests/ice cream.png";
+import CHEESE from "../../assests/cheese pizza.png";
+import Burger from "../../assests/burger.png";
 
-function Hover(){
-    return(
-     <div className="Hover-box">
+function Hover() {
+  return (
+    <div className="Hover-box">
+      <div className="text">
+        <h3>Fresh from pizzon </h3>
+        <h1>OUR SPECIALITY</h1>
+      </div>
+      <div className="items">
         <div className="Hover1">
-
+          <img src={ICE} className="imgtag" ></img>
+          <h2>Desert</h2>
         </div>
-        <div className="Hover2">
-
+        <div className="Hover2" >
+          <img src={CHEESE} className="imgtag"></img>
+          <h2>Cheese Pizza</h2>
         </div>
-        <div className="Hover3">
-            
+        <div className="Hover3" >
+          <img src={Burger} className="imgtag" ></img>
+          <h2>Burger</h2>
         </div>
-     </div>
-    );
+      </div>
+      <div className="but">
+     <a href="#"> <button id="butt">View More</button></a>
+      </div>
+      
+    </div>
+  );
 }
 
 export default Hover;
